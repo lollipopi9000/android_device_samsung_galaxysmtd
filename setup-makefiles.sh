db3464ba3ddd271769c81ed97801dd292deb6533
jeffreyvh <jeffrey25011998@gmail.com>
2015-04-27 07:52:56 +0200

    Update setup-makefiles.sh
    Removed Live-Wallpaper support and also the live wallpapers

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -64,15 +64,7 @@ echo "    $OUTDIR/proprietary/modem.bin:modem.bin" >> ../../../$OUTDIR/$DEVICE-v
 
 # Live wallpaper packages and Themes
 PRODUCT_PACKAGES := \\
-    LiveWallpapers \\
-    LiveWallpapersPicker \\
-    MagicSmokeWallpapers \\
-    VisualizationWallpapers \\
     librs_jni
-
-# Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \\
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/aries-common/overlay
