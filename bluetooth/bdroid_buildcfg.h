7b85f108e8cb670e60d38a3aa68cce7af821423e
Coldwindofnowhere <coldwindofnowhere@gmail.com>
2016-08-30 10:01:15 +0200

    Fix bt

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -26,5 +26,7 @@
 #define SMP_INCLUDED FALSE
 #define REMOVE_EAGER_THREADS FALSE
 
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 
 #endif
